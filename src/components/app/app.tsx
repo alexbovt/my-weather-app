948741e1bce93172e033e27735ea2189ca227b30
alexbovt <bovttt@gmail.com>
2019-05-13 21:55:24 +0200

    Initial commit

---
+++ src/components/app/app.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+        <Button variant="contained" color="secondary">
+          Hello World
+        </Button>
+        <Button variant="contained" color="default">
+          Hello World
+        </Button>
+      </div>
+    );
+  }
+}
---
+++ src/components/search-panel/search-panel.tsx
@@ -0,0 +1,6 @@
+import react, {Component} from 'react'
+
+
+export default class SearchPanel extends Component {
+    
+}
